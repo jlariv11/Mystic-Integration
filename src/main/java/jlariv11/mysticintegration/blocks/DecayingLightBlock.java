6c47deb612216c7219e5b5989da768f3315cca2a
jake <jlariv95@gmail.com>
2021-07-01 17:47:25 -0400

    function to decay light value by amount

--- src/main/java/jlariv11/mysticintegration/blocks/DecayingLightBlock.java
+++ src/main/java/jlariv11/mysticintegration/blocks/DecayingLightBlock.java
@@ -1,8 +1,11 @@
 package jlariv11.mysticintegration.blocks;
 
+import javafx.beans.property.StringProperty;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
@@ -14,16 +17,15 @@ public class DecayingLightBlock extends BaseBlock{
         this.light = light;
     }
 
-
+    public void decayLight(int amount){
+        this.light -= amount;
+    }
 
     @Override
     public int getLightValue(BlockState state, IBlockReader world, BlockPos pos) {
         return light;
     }
 
-    private void setTexture(BlockState state){
-        IBakedModel baseModel = Minecraft.getInstance().getBlockRenderer().getBlockModel(state);
-    }
-
+    
 
 }
