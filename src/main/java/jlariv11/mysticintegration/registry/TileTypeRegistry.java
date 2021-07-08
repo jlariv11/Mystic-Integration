1caa2952329c075d6d9fb0dfe4994f2766f2f0a1
jake <jlariv95@gmail.com>
2021-07-08 17:45:13 -0400

    add cable to test power gen/transfer

--- src/main/java/jlariv11/mysticintegration/registry/TileTypeRegistry.java
+++ src/main/java/jlariv11/mysticintegration/registry/TileTypeRegistry.java
@@ -1,6 +1,7 @@
 package jlariv11.mysticintegration.registry;
 
 import jlariv11.mysticintegration.MysticIntegration;
+import jlariv11.mysticintegration.blocks.tiles.CableTile;
 import jlariv11.mysticintegration.blocks.tiles.ChargerTile;
 import jlariv11.mysticintegration.blocks.tiles.GeneratorTile;
 import net.minecraft.tileentity.TileEntityType;
@@ -18,6 +19,9 @@ public class TileTypeRegistry {
     public static final RegistryObject<TileEntityType<ChargerTile>> CHARGER_TILE = TILE_TYPES.register(
             "charger_tile", () -> TileEntityType.Builder.of(ChargerTile::new, BlockRegistry.CHARGER.get())
             .build(null));
+    public static final RegistryObject<TileEntityType<CableTile>> CABLE_TILE = TILE_TYPES.register(
+            "cable_tile", () -> TileEntityType.Builder.of(CableTile::new, BlockRegistry.TEMP_CABLE.get())
+                    .build(null));
 //    public static final RegistryObject<TileEntityType<DecayingLightTile>> DECAYING_LIGHT = TILE_TYPES.register(
 //            "decaying_light_tile", () -> TileEntityType.Builder.of(DecayingLightTile::new,
 //                    BlockRegistry.DECAYING_LIGHT_BLOCK.get()).build(null));
