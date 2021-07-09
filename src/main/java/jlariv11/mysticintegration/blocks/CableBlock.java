f91931ec7544c87613246d5db13e2cd6ec14fd04
jake <jlariv95@gmail.com>
2021-07-09 14:42:47 -0400

    new cable model and texture

--- src/main/java/jlariv11/mysticintegration/blocks/CableBlock.java
+++ src/main/java/jlariv11/mysticintegration/blocks/CableBlock.java
@@ -1,8 +1,12 @@
 package jlariv11.mysticintegration.blocks;
 
 import jlariv11.mysticintegration.blocks.tiles.CableTile;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
@@ -15,4 +19,11 @@ public class CableBlock extends TestPowerBlock{
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new CableTile();
     }
+
+    @Override
+    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
+        return Block.box(0, 8, 6.0D, 16.0D, 12.0D, 10.0D);
+    }
+
+
 }
